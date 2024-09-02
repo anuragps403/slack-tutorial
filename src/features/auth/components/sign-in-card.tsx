6c7952ad18d91db8c5d6389460cdb2761f71205f
Anurag Singh <anuragps2096@gmail.com>
2024-09-02 16:00:46 -0700

    Convex setup

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -19,7 +19,9 @@ export const SignInCard = ({setState}: SignInCardProps) => {
     return (
         <Card className="w-full h-full p-8">
             <CardHeader className="px-0 pt-0">
-                <CardTitle>Login to continue</CardTitle>
+                <CardTitle>
+                    Login to continue
+                </CardTitle>
                 <CardDescription>
                     Use your email or another service to continue
                 </CardDescription>
