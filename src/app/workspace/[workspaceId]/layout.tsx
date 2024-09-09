97d04070e35117fd10446bc9697bd9d3b4e41221
Anurag Singh <anuragps2096@gmail.com>
2024-09-08 21:49:08 -0700

    Added sidebar on workspace route, implemented workspaceswitcher, sidebarbutton component to be used in siderbar. Also used User button in sider bar

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Sidebar } from "./sidebar";
 // In this case the children component is not neccary will be client component (Bery good next js behavior, i think it with only layout case)
 
 import { Toolbar } from "./toolbar";
@@ -11,9 +12,12 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
   return (
     <div className="h-full">
         <Toolbar />
-        {children}
+        <div className="flex h-[calc(100vh-40px)]">
+            <Sidebar />
+            {children}
+        </div>
     </div>
-  )
-}
+  );
+};
 
 export default WorkspaceIdLayout;
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,14 +1,6 @@
-"use client"
-
-import { useGetWorkspace } from "@/features/workspace/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-
 const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
-
   return (
-    <div>Data: { JSON.stringify(data) }</div>
+    <div>Workspace id page here</div>
   )
 };
 
